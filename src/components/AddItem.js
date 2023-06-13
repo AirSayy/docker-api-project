dc03cdc0ce15df88d66c0832ae2900acd04a324c
Eseoghene Eguakhide <eruotorese@yahoo.com>
2023-06-13 14:50:19 +0100

    style item component

--- src/components/AddItem.js
+++ src/components/AddItem.js
@@ -1,88 +1,3 @@
-// import React, { useState } from 'react';
-// import { useNavigate } from 'react-router-dom';
-// import { createItem } from './api';
-
-
-// function AddItem() {
-//     const [newItemData, setNewItemData] = useState({
-//         name: '',
-//         description: '',
-//         parentId: '',
-//     });
-//     const navigate = useNavigate();
-
-//     const handleCreateItem = async (e) => {
-//         e.preventDefault();
-    
-//         try {
-//           const response = await createItem(newItemData);
-
-//           setNewItemData({ name: '', description: '', parentId: '' });
-
-//           navigate('/tableView', { state: { createdItemId: response.data.id } });
-         
-//         //   loadTableData();
-//           console.log(newItemData)
-//         } catch (error) {
-//           console.error('Error creating item:', error);
-//         }
-//     };
-   
-            
-        
-
-   
-
-//   return (
-//     <>
-   
-//     <form className='m-auto w-[50%]' onSubmit={handleCreateItem} >
-//         <div className="mb-6">
-//             <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Name :</label>
-//             <input
-//                 className="shadow-sm bg-white border border-black text-black text-sm rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2.5 dark:bg-white dark:border-black dark:placeholder-gray-400 dark:text-black dark:focus:ring-gray-500 dark:focus:border-gray-500 dark:shadow-sm-light"
-//                 type="text"
-//                 placeholder="Name"
-//                 value={newItemData.name}
-//                 onChange={(e) =>
-//                     setNewItemData({ ...newItemData, name: e.target.value })
-//                 }
-//             />
-//         </div>
-//         <div className="mb-6">
-//             <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Description :</label>
-//             <textarea
-//                 className="shadow-sm bg-white border border-black text-black text-sm rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2.5 dark:bg-white dark:border-black dark:placeholder-gray-400 dark:text-black dark:focus:ring-gray-500 dark:focus:border-gray-500 dark:shadow-sm-light"
-//                 placeholder="Description"
-//                 value={newItemData.description}
-//                 onChange={(e) =>
-//                     setNewItemData({ ...newItemData, description: e.target.value })
-//                 }
-//             />
-//         </div>
-//         <div className="mb-6">
-//             <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Parent Id :</label>
-//             <input
-//                 className="shadow-sm bg-white border border-black text-black text-sm rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2.5 dark:bg-white dark:border-black dark:placeholder-gray-400 dark:text-black dark:focus:ring-gray-500 dark:focus:border-gray-500 dark:shadow-sm-light"
-//                 type="text"
-//                 placeholder="Parent"
-//                 value={newItemData.parentId}
-//                 onChange={(e) =>
-//                     setNewItemData({ ...newItemData, parentId: e.target.value })
-//                 }
-//             />
-//         </div>
-         
-//         <button className="text-white bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800" type='submit'>Create Item</button>
-        
-        
-//       </form>
-//       </>
-//   )
-// }
-
-// export default AddItem
-
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { createItem, getItems } from './api';
@@ -128,25 +43,31 @@ const AddItem = () => {
 
   return (
     <div>
-      <h2>Add Item</h2>
-      <form>
-        <label htmlFor="name">Name:</label>
-        <input type="text" id="name" name="name" value={itemData.name} onChange={handleInputChange} />
+      
+      <form className='m-auto w-[50%]'>
 
-        <label htmlFor="description">Description:</label>
-        <textarea id="description" name="description" value={itemData.description} onChange={handleInputChange} />
+      <div className="mb-6">
+        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-black" htmlFor="name">Name:</label>
+        <input className="shadow-sm bg-white border border-black text-black text-sm rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2.5 dark:bg-white dark:border-black dark:placeholder-gray-400 dark:text-black dark:focus:ring-gray-500 dark:focus:border-gray-500 dark:shadow-sm-light" type="text" id="name" name="name" value={itemData.name} onChange={handleInputChange} />
+      </div>
+      <div className="mb-6">
+        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-black" htmlFor="description">Description:</label>
+        <textarea className="shadow-sm bg-white border border-black text-black text-sm rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2.5 dark:bg-white dark:border-black dark:placeholder-gray-400 dark:text-black dark:focus:ring-gray-500 dark:focus:border-gray-500 dark:shadow-sm-light" id="description" name="description" value={itemData.description} onChange={handleInputChange} />
+        </div>
 
-        <label htmlFor="parentId">Parent:</label>
-        <select id="parentId" name="parentId" value={itemData.parentId} onChange={handleInputChange}>
-          <option value="">None</option>
+        <div className="mb-6">
+        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-black" htmlFor="parentId">Parent:</label>
+        <select className="shadow-sm bg-white border border-black text-black text-sm rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2.5 dark:bg-white dark:border-black dark:placeholder-gray-400 dark:text-black dark:focus:ring-gray-500 dark:focus:border-gray-500 dark:shadow-sm-light" id="parentId" name="parentId" value={itemData.parentId} onChange={handleInputChange}>
+          {/* <option value="">None</option> */}
           {validItems.map((item) => (
             <option key={item.id} value={item.id}>
               {item.name}
             </option>
           ))}
         </select>
+        </div>
 
-        <button type="button" onClick={handleAddItem}>Add</button>
+        <button className="text-white bg-black hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-gray-900 dark:hover:bg-gray-700 dark:focus:ring-gray-800" type="button" onClick={handleAddItem}>Add Item</button>
       </form>
     </div>
   );
